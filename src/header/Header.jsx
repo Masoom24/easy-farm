3c04dc086d155c48339046d717638b427a36f427
IAmAnshul07 <anshulpatidar1298@gmail.com>
2022-12-29 12:35:24 +0530

    Added header and other styling

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import '../header/HeaderCSS';
 const Header = () =>{
     return(
         <>
 
-    <div className='flex justify-between p-5 border border-red-500'>
+    <div className='flex justify-between p-5 border border-red-500 text-yellow-800'>
         <div>Logo</div>
-        <div className='flex justify-between w-1/4'>
-            <div><Link to='/home'>Home</Link></div>
+        <div className='flex justify-between w-1/4 clear-none list-none'>
+            <div><li className='headerUnderlineEffect hover:underline   hover:decoration-violet-900'><Link to='/home'>Home</Link></li></div>
             <div>Contact us</div>
             <div>About US</div>
         </div>
-        <div>
+        <div className='flex text-orange-500'>
             <input type="search" name="" id="" className='border border-black px-2 py-1 focus:outline-0 hover:rounded-2xl' placeholder='Search Items'/>
         </div>
         <div>Profile</div>
