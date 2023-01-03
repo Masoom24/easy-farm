1bcf428300a7147f329eaaab2a941be89767764a
Ritik Patidar <67598349+Ritik-Patidar@users.noreply.github.com>
2023-01-03 23:15:17 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>This is the main landing page!!</div>,
+    element: <div>This is the New landing page!!</div>,
   },
   {
     path: "/home",
