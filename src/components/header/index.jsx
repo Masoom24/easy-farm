64e280e2243399fc7e2cb3641f74f1496ea6e730
IAmAnshul07 <anshulpatidar1298@gmail.com>
2023-01-03 15:16:49 +0530

    Changes in header color

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,15 +7,15 @@ const Header = () => {
     return (
         <>
 
-            <div className='flex justify-between p-5 border drop-shadow-lg bg-mintcream-400 text-white'>
+            <div className='flex justify-between p-5 border drop-shadow-lg bg-cyan-400 text-white bg-gradient-to-l from-purple-400 to cyan-500'>
                 <div><img src={homesvg} alt="" className='w-10' /></div>
                 <div className='flex justify-between w-1/4 clear-none list-none'>
-                    <div><li className='drop-shadow-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 rounded-xl p-2'><Link to='/home'>Home</Link></li></div>
-                    <div><li className='drop-shadow-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 rounded-xl p-2'><Link to='/contactUs'>Contact us</Link></li></div>
-                    <div><li className='drop-shadow-lg transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 rounded-xl p-2'><Link to='/aboutUs'>About us</Link></li></div>
+                    <div><li className='drop-shadow-lg transition ease-in-out delay-150 bg-amber-500 hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300 rounded-xl p-2'><Link to='/home'>Home</Link></li></div>
+                    <div><li className='drop-shadow-lg transition ease-in-out delay-150 bg-amber-500 hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300 rounded-xl p-2'><Link to='/contactUs'>Contact us</Link></li></div>
+                    <div><li className='drop-shadow-lg transition ease-in-out delay-150 bg-amber-500 hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300 rounded-xl p-2'><Link to='/aboutUs'>About us</Link></li></div>
                 </div>
                 <div className='flex text-white'>
-                    <input type="search" name="" id="" className='border-2 border-blue-500 px-2 py-1 focus:outline-0 rounded-2xl bg-blue-500 drop-shadow-lg hover:bg-indigo-500' placeholder='Search Items' />
+                    <input type="search" name="" id="" className='border-2 border-yellow-500 px-2 py-1 focus:outline-0 rounded-2xl bg-amber-500 drop-shadow-lg hover:bg-orange-500' placeholder='Search Items' />
                 </div>
                 <div><img src={profilePNG} className='w-10 drop-shado-lg' alt="" /></div>
             </div>
