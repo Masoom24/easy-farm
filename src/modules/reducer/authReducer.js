a25d48f3c5d1306cfd85a233571b2ff820fc93e0
IAmAnshul07 <anshulpatidar1298@gmail.com>
2023-01-16 15:59:44 +0530

    Refresh handling

--- src/modules/reducer/authReducer.js
+++ src/modules/reducer/authReducer.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 const initialState = {
     isLoading: false,
-    isAuth : false ,
-    userData : {
-        
+    isAuth: false,
+    userData: {
+
     },
     value: 0
 }
@@ -16,63 +16,70 @@ const initialState = {
 
 // }
 
-export const register = createAsyncThunk('auth/register', async({data},rejectWithValue) => {
-    
-    try{
-        const res = await axios.post('https://major-backend.vercel.app/v1/auth/register',data);
-        console.log(res.data);
-        localStorage.setItem('userData',JSON.stringify(res.data));
-        return res.data ;
-    }catch(err){
-        return rejectWithValue(err) ;
-    }
-});
+export const register = createAsyncThunk('auth/register', async ({ data }, rejectWithValue) => {
 
-export const login = createAsyncThunk('auth/login', async({data},rejectWithValue) =>{
-    try{
-        const res = await axios.post('https://major-backend.vercel.app/v1/auth/login',data);
+    try {
+        const res = await axios.post('https://major-backend.vercel.app/v1/auth/register', data);
         console.log(res.data);
         localStorage.setItem('userData', JSON.stringify(res.data));
         return res.data;
-    }catch(err){
+    } catch (err) {
+        return rejectWithValue(err);
+    }
+});
+
+export const login = createAsyncThunk('auth/login', async ({ data }, rejectWithValue) => {
+    try {
+        const res = await axios.post('https://major-backend.vercel.app/v1/auth/login', data);
+        console.log(res.data);
+        localStorage.setItem('userData', JSON.stringify(res.data));
+        return res.data;
+    } catch (err) {
         return rejectWithValue(err);
     }
 })
+
+
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
-        
+        authSuccess: (state, { payload }) => ({
+            ...state,
+            isLoading: false,
+            isAuth: true,
+            user: payload.user
+        })
     },
     extraReducers: (builder) => {
-        builder.addCase(register.pending , (state) => {
-            state.isLoading = true ;
-        })
-        .addCase(register.fulfilled, (state , {payload}) => {
-            state.isLoading = false;
-            state.isAuth = true;
-            state.userData = payload.user;
-        })
-        .addCase(register.rejected, (state, {payload})=>{
-            state.isLoading = false;
-            state.isAuth = false;
-            state.userData = initialState.userData;
-        })
-        .addCase(login.pending, (state, {payload}) =>{
-            state.isAuth = false;
+        builder.addCase(register.pending, (state) => {
             state.isLoading = true;
         })
-        .addCase(login.fulfilled, (state, {payload})=>{
-            state.isAuth = true;
-            state.isLoading = false;
-            state.userData = payload.user;
-        })
-        .addCase(login.rejected, (state, {payload})=>{
-            state.isAuth = false;
-            state.isLoading = false;
-            state.userData = initialState.userData;
-        })
+            .addCase(register.fulfilled, (state, { payload }) => {
+                state.isLoading = false;
+                state.isAuth = true;
+                state.userData = payload;
+            })
+            .addCase(register.rejected, (state, { payload }) => {
+                state.isLoading = false;
+                state.isAuth = false;
+                state.userData = initialState.userData;
+            })
+            .addCase(login.pending, (state, { payload }) => {
+                state.isAuth = false;
+                state.isLoading = true;
+            })
+            .addCase(login.fulfilled, (state, { payload }) => {
+                state.isAuth = true;
+                state.isLoading = false;
+                state.userData = payload;
+            })
+            .addCase(login.rejected, (state, { payload }) => {
+                state.isAuth = false;
+                state.isLoading = false;
+                state.userData = initialState.userData;
+            })
     }
 });
 
@@ -83,6 +90,6 @@ const authSlice = createSlice({
 //     }
 // }
 //!  Exporting ACTIONS
-export const { increment, decrement } = authSlice.actions;
+export const { authSuccess } = authSlice.actions;
 
 export default authSlice.reducer;
