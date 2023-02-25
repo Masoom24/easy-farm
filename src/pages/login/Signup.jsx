432c7e62984dbff83931582b1b179d4804d79187
IAmAnshul07 <anshulpatidar1298@gmail.com>
2023-02-25 18:33:12 +0530

    setting user

--- src/pages/login/Signup.jsx
+++ src/pages/login/Signup.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom';
 import { register } from '../../modules/reducer/authReducer';
 import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 const Signup = () => {
     const divStyles = {
         boxShadow: '1px 2px 5px #1A237E',
@@ -19,15 +20,30 @@ const Signup = () => {
         alignItems: 'center',
     };
     const dispatch = useDispatch();
+    const [userData,setUserData] = useState({
+        email:"",
+        password:"",
+        name:"",
+        role:"seller"
+    })
+
+    const handleChange = (e) => {
+        const { name , value } = e.target ;
+        setUserData((pre) => ({
+            ...pre,
+            [name]:value
+        }))
+    }
+
     const clickedButton = (e) =>{
         e.preventDefault();
-        const data = {
-            name:'anshul',
-            email: 'anshulpa@gmail.com',
-            password : '1234567A90',
-            role: "seller"
-        }
-        dispatch(register({data}));
+        // const data = {
+        //     name:'anshul',
+        //     email: 'anshulpa@gmail.com',
+        //     password : '1234567A90',
+        //     role: "seller"
+        // }
+        dispatch(register({data:userData}));
     } 
     return (
         <div>
@@ -36,13 +52,13 @@ const Signup = () => {
                         <div className="container">
                             <h2 style={{ color: '#2ccce4' }} className="text-3xl font-bold">Sign-up</h2>
                             <div>
-                                <input type="text" placeholder="Name" style={formFields} />
+                                <input value={userData.name} name="name" onChange={handleChange} type="text" placeholder="Name" style={formFields} />
                             </div>
                             <div >
-                                <input type="email" placeholder="Email" style={formFields} />
+                                <input value={userData.email} name="email" onChange={handleChange} type="email" placeholder="Email" style={formFields} />
                             </div>
                             <div >
-                                <input type="password" placeholder="Password" style={formFields} />
+                                <input value={userData.password} name="password" onChange={handleChange} type="password" placeholder="Password" style={formFields} />
                             </div>
                             <div>
                                 <button type="submit" className="btn" class="bg-gradient-to-r from-purple-400 to cyan-500 hover:from-pink-500 hover:to-orange-500 text-white font-semibold px-6 py-3 rounded-md mr-6" >Signup</button>
