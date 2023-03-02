bf484881d6e2c85b53777fed1a70fdf310f069df
IAmAnshul07 <anshulpatidar1298@gmail.com>
2023-03-02 21:14:54 +0530

    Logout Functionality

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import profile from '../../asset/Icons/profile.gif';
+// import profile from '../../asset/Icons/profile.gif';
 import logo from '../../asset/Icons/Logos/png/ColorLogo.png';
 //import logo from '../../asset/Icons/Logos/png/White logo - no background.png';
 import addToBasket from '../../asset/Icons/addToBasket.gif'
-import logoutButtonStatic from '../../asset/Icons/logoutButtonStatic.png'
+import { Button } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../modules/reducer/authReducer';
+// import logoutButtonStatic from '../../asset/Icons/logoutButtonStatic.png'
+// import { Button } from '@mui/material';
 
 const Header = () => {
-    
+
+    const dispatch = useDispatch(); 
+    const clickedLogout = (e) =>{
+        e.preventDefault();
+        dispatch(logout());
+    }
     return (
         <>
 
@@ -23,8 +32,8 @@ const Header = () => {
                     <input type="search" name="" id="" className='border-2 bg-transparent h-8 cursor-pointer text-blue-500 border-yellow-500 px-2 py-1 my-2 focus:outline-0 rounded-2xl drop-shadow-lg ' placeholder='Search Items' />
                 </div>
                 <div className='flex justify-center items-center'><Link to='/addProduct'><img src={addToBasket} className='w-16 drop-shado-lg' alt="logo" /></Link></div>
-                <div className='flex justify-center items-center'><Link to='/profile'><img src={profile} className='w-16 drop-shado-lg' alt="logo" /></Link></div>
-                <div className='flex justify-center items-center'><Link to='/profile'><img src={logoutButtonStatic} className='w-16 drop-shado-lg' alt="logo" /></Link></div>
+                {/* <div className='flex justify-center items-center'><Link to='/profile'><img src={profile} className='w-16 drop-shado-lg' alt="logo" /></Link></div> */}
+                <div className='flex justify-center items-center'><li className='list-none drop-shadow-lg transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 hover:bg-orange-500 duration-300 rounded-xl'><Link to="/home" className='no-underline'><Button onClick={clickedLogout}>Logout</Button></Link></li></div>
             </div>
 
         </>
