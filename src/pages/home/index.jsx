ecd49c6f62a85f32d7b77a513c73dd87aae8243c
krutika <krutikasapkale@gmail.com>
2023-03-08 09:45:59 +0530

    heading removed and position fixed

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -24,10 +24,8 @@ const Home = () => {
             <h1 style={{ color: "black" }} className="text-3xl md:text-6xl lg:text-8xl uppercase font-black mb-8">EasyFarm</h1>
 
             <p style={{ color: "#002984" }} className="text-base md:text-lg lg:text-2xl mb-8 my-3">A place for Buyers and Sellers to find tools related to agriculture!</p>
-
-            <div style={{ color: "#E74C3C" }} className=" flex justify-center items-center ml-[22vw] my-3 bg-gradient-to-r from-black to gray-900 hover:from-pink-500 hover:to-orange-500 text-white font-semibold py-3 rounded-md w-50 ">
-              <h1 className='flex justify-center items-center py-2 w-1/2 uppercase text-bold text-xl'>Top Offers</h1>
-            </div><Sliders /><br />
+             <br />
+            <Sliders /><br />
             <div>
               <Link to='/signup'> <Button class="bg-gradient-to-r from-black to gray-400 hover:from-gray-500 hover:to-black text-white font-semibold px-20 py-3 rounded-md mr-6">
                 <center>Register</center>
