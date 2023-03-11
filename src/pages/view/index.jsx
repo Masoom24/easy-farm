22a1ade59bd142d74afdce58c341cf0eb6dcdb3b
krutika <krutikasapkale@gmail.com>
2023-03-11 18:52:59 +0530

    product data added

--- src/pages/view/index.jsx
+++ src/pages/view/index.jsx
@@ -2,7 +2,38 @@ function View() {
     return (
         <div className="viewcontainer">
             <div className="row">
-                
+        <CardView
+          title="Power Weeders"
+          images={v1}
+          Price="9999"
+          rupess="&#x20B9;"
+          alt="a"
+         
+        />
+        <CardView
+          title="Power Weeders"
+          images={v5}
+          Price="500"
+          rupess="&#x20B9;"
+          alt="b"
+          
+        />
+        <CardView
+          title="stihl power Weeder"
+          images={v3}
+          Price="7000"
+          rupess="&#x20B9;"
+          alt="c"
+         
+        />
+        <CardView
+          title="Lawn Mowers"
+          images={v4}
+          Price="500"
+          rupess="&#x20B9;"
+          alt="d"
+         
+        />
             </div>
         </div>
         );
