d842a7610328595eb543b0fed94d2ff12e4e0288
krutika <krutikasapkale@gmail.com>
2023-03-12 12:42:45 +0530

    rent page added

---
+++ src/pages/rentproduct/index.jsx
@@ -0,0 +1,10 @@
+function Rentproduct() {
+    return (
+      <div className="viewcontainerR">
+        <div className="rowR">
+          
+        </div>
+      </div>
+    );
+  }
+  export default Rentproduct;
