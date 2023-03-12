7c0900960a4398aeaaae35de37d50ff5612c5269
krutika <krutikasapkale@gmail.com>
2023-03-12 12:43:18 +0530

    product data added

--- src/pages/rentproduct/index.jsx
+++ src/pages/rentproduct/index.jsx
@@ -2,7 +2,38 @@ function Rentproduct() {
     return (
       <div className="viewcontainerR">
         <div className="rowR">
-          
+        <Rent_Card
+            title="product 1"
+            images={p7}
+            Price="9999"
+            rupess="&#x20B9;"
+            alt="a"
+           
+          />
+          <Rent_Card
+            title="product 2"
+            images={p6}
+            Price="500"
+            rupess="&#x20B9;"
+            alt="b"
+            
+          />
+          <Rent_Card
+            title="product 3"
+            images={p5}
+            Price="7000"
+            rupess="&#x20B9;"
+            alt="c"
+           
+          />
+          <Rent_Card
+            title="product 4"
+            images={p4}
+            Price="500"
+            rupess="&#x20B9;"
+            alt="d"
+           
+          />
         </div>
       </div>
     );
