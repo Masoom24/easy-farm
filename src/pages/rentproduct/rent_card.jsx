d048cd2673351c52e11203d2f329cbcb07c95916
krutika <krutikasapkale@gmail.com>
2023-03-12 12:45:40 +0530

    some changes in card code

--- src/pages/rentproduct/rent_card.jsx
+++ src/pages/rentproduct/rent_card.jsx
@@ -19,7 +19,17 @@ export default function Rent_Card(props) {
                            
                             <p className="Rprice newPrice">{rupess}{Price}</p>
                         </div>
-                       
+                        <button className="rentButton">Rent</button>
+                        <div className="Rcart">
+                        <svg className="RoutCart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+                        <Link to='/viewProducts'>
+                                <path d="M2 6h10l10 40h32l8-24H16"></path>
+                                <circle cx="23" cy="54" r="4"></circle>
+                                <circle cx="49" cy="54" r="4"></circle>
+                                </Link>
+                            </svg>
+                            
+                        </div>
                     </div>
                 </div>
             </div>
